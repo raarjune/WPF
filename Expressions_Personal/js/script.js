4b4a13d1e44559fab7bf31468b8a4ad4595e1cac
raarjune <steffyarjune@gmail.com>
2013-12-01 22:50:59 -0500

    Adding personal expression

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,1 +1,12 @@
-//Rajmatie Arjune, November 29, 2013. Expressions_Personal
+//Rajmatie Arjune. December 1, 2013. Expressions_Personal
+//How many lipgloss would I use per year, based upon months used?.
+
+var lipgloss = 5.00;
+var month = 1;
+var year = month;
+ 
+var lipgloss = prompt ("How many lipgloss do you use per month?");
+var month = prompt ("How many months do you use lipgloss for the year?")
+
+console.log(month*lipgloss);
+
