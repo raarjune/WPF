75320b199d3295492d5fa31bfcf40aca1c19beca
raarjune <steffyarjune@gmail.com>
2013-12-01 21:36:33 -0500

    Income-Expenditure spreadsheet size.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,1 +1,15 @@
 //Rajmatie Arjune, November 29, 2013. Expressions_Industry
+//Income-Expenditure spreadsheet
+
+var column = 0;
+var row = 0;
+var area = column*row;
+
+//console.log(column * row);
+
+var column = prompt ("Please enter number of columns needed");
+var row = prompt ("Please enter number of rows needed");
+
+console.log(column*row);
+console.log("Thank you! Your requested total is listed above");
+
