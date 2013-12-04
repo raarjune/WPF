4474973717b101cc24f78e13e55b5b1c077ded4e
raarjune <steffyarjune@gmail.com>
2013-12-03 23:01:48 -0500

    Updating calculation

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,14 +3,16 @@
 
 var age = 0;
 var candy = 12;
+var calories = candy * 12;
 
 //Calories contained in one piece of candy is 12.
 
 var age = prompt ("Please enter your age.");
+
 var calories = prompt ("How many candy do you consume per day?");
 
-console.log(candy*age);
+console.log(candy*age*calories);
 
 //If a person consume candies based upon their age. Age multiply by 12 calories per candy would result based upon info gathered.
 
-alert ("You would have consume the total calories " +candy*age);
+alert ("Your total calories intake would be " +candy*age*calories);
