d2b63b0bb2666bcc14a3f6616fb61c7f9132a0a9
raarjune <steffyarjune@gmail.com>
2013-12-03 22:49:11 -0500

    Wacky expressions

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,16 @@
-//Rajmatie Arjune, November 29, 2013. Expressions_Wacky
+//Rajmatie Arjune. December 3, 2013. Expressions_Wacky
+//Calculating the amount of calories a person would intake based upon candies consumed.
+
+var age = 0;
+var candy = 12;
+
+//Calories contained in one piece of candy is 12.
+
+var age = prompt ("Please enter your age.");
+var calories = prompt ("How many candy do you consume per day?");
+
+console.log(candy*age);
+
+//If a person consume candies based upon their age. Age multiply by 12 calories per candy would result based upon info gathered.
+
+alert ("You would have consume the total calories " +candy*age);
