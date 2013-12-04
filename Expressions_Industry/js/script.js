6692cd225d3d83f89c89b29327a903f57857f4d7
raarjune <steffyarjune@gmail.com>
2013-12-03 21:38:24 -0500

    Adding comments

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -10,6 +10,7 @@ var area = column*row;
 var column = prompt ("Please enter number of columns needed");
 var row = prompt ("Please enter number of rows needed");
 
+//collecting information from user to create spreadsheet. The information calculated would print the total cells for spreadsheet.
 console.log(column*row);
 console.log("Thank you! Your requested total is listed above");
 
