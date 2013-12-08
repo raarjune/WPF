ab4f382d8013040b3108c24ed2799b24f3955a2d
raarjune <steffyarjune@gmail.com>
2013-12-08 11:49:49 -0500

    Updating condition

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,9 +1,15 @@
 //Rajmatie Arjune, December 8 2013. Conditionals_Personal
 
  //If we have $25.00 and over we will buy a cake.
-var cake = 25;
+var cake = 35;
 //If we collect 25 dollars we will buy a cake.
 if(cake > 25){
+    var candy = 1
+    if(cake > 25) {
+        //candy()
+        //if we have extra cash will buy candy too
+        console.log("Get cake and candy");
+    }
 console.log("We will buy a cake.")
 }else{
 //If it is less than we will buy a pie.
