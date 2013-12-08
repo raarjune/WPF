b213ea808db8f90bd9797e4f98304eb5c35f3810
raarjune <steffyarjune@gmail.com>
2013-12-08 11:32:57 -0500

    Statement

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,1 +1,11 @@
 //Rajmatie Arjune, December 8 2013. Conditionals_Personal
+
+ //If we have $25.00 and over we will buy a cake.
+var cake = 25;
+//If we collect 25 dollars we will buy a cake.
+if(cake > 25){
+console.log("We will buy a cake.")
+}else{
+//If it is less than we will buy a pie.
+console.log("We will buy a pie");
+}
