aaab17c3e06d0d23d9086fcf4287de644a0abdf7
raarjune <steffyarjune@gmail.com>
2013-12-10 21:17:36 -0500

    Conditionals Industry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,1 +1,9 @@
-//Rajmatie Arjune, December 18 2013. Conditionals_Industry
+//Rajmatie Arjune, December 10 2013. Conditionals_Industry
+//Office supplies for employees
+var supplies = 6;
+//Employees are allowed to have a total of 6 items only.
+//If more than 6 items are ordered, no supplies would be authorized.
+
+var pen = 2;
+var handsanitizer = 2;
+var facialtissue = 2;
