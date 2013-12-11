149c5be2f7567bbf79f5af9a8d567e2840818755
raarjune <steffyarjune@gmail.com>
2013-12-10 21:18:32 -0500

    condition

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -7,3 +7,13 @@ var supplies = 6;
 var pen = 2;
 var handsanitizer = 2;
 var facialtissue = 2;
+
+//office supplies would be multiply by the quantity needed
+var price = 2;
+
+var supplies = prompt("How many items do you need?");
+
+if(supplies > 6){
+    
+    console.log("Sorry, no supplies at this time.");
+    
