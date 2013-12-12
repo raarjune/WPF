aa14cd33be15c5c02e417e9346f43eaa5b14f7de
raarjune <steffyarjune@gmail.com>
2013-12-11 22:00:04 -0500

    Updates

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -7,11 +7,12 @@ var flight = 0;
 var flight = prompt("What price did you find?");
 
 if (flight > 495) {
-    console.log("Train would be your best option, and you could take as many luggage as you want.")
+    console.log("Train is a good option, and you could take as many bags as you want.")
     //code
 }else{
     console.log("Flying would get you to DC in 2 hours. 1 bag check-in allowed");
 }
 alert("Have a pleasant trip to Washington!");
+alert("Hotels could be booked by visiting www.hotels.com")
 
 
