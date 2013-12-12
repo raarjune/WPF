a59b43b6428219ddd73a145c42fca69c3c0c0d27
raarjune <steffyarjune@gmail.com>
2013-12-11 21:10:15 -0500

    Planning trip

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,2 +1,6 @@
-//Rajmatie Arjune, December 10 2013. Conditionals_Wacky
+//Rajmatie Arjune, December 11 2013. Conditionals_Wacky
+
+//Planning a trip to Washington DC.
+//Based upon the price for flight if less than $495. will fly, if greater will travel by train.
+
 
