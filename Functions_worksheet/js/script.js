5e8d62e840de0a15deea141fe71ca4fdc3a2ef0a
raarjune <steffyarjune@gmail.com>
2013-12-15 21:40:24 -0500

    Functions_worksheet

---
+++ Functions_worksheet/js/script.js
@@ -0,0 +1,11 @@
+//Rajmatie Arjune, December 15, 2013 - Functions_worksheet
+
+//Calculate the Area of Rectangle
+
+var width = 5;
+var height = 6;
+var area = (width * height);
+//var rectangle = width*height;
+
+console.log("The Area of the Rectangle is " + area);
+
