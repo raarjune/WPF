d4ae612b772c9bdf64dfad094bff152d1858ee88
raarjune <steffyarjune@gmail.com>
2013-12-17 06:10:59 -0500

    Budget

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,6 +1,15 @@
 //Rajmatie Arjune, December 15, 2013 - Functions_Industry
 
+//Preparing budget for everday expense.
 
+function budget(){
+    var pettyCash = 250;
+    var month = 12;
+
+    var annualBudget = pettyCash * month;
+    console.log(annualBudget);
+}
+budget();
 
 
 
