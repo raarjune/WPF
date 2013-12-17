f88149684331c73680876dcab1c1e772e338377e
raarjune <steffyarjune@gmail.com>
2013-12-17 06:07:05 -0500

    .

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,1 +1,9 @@
 //Rajmatie Arjune, December 8 2013. Conditionals_Worksheet
+
+ function calcArea(){
+     var width = 20;
+     var height = 30;
+     var area = width * height;
+     console.log(area);
+ }
+ calcArea();
