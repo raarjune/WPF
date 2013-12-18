5a94565a535fd59de705f554a601ddf6cab1325f
raarjune <steffyarjune@gmail.com>
2013-12-17 20:45:38 -0500

    Industry functions

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -9,10 +9,14 @@ function budget(){
     var annualBudget = pettyCash * month;
     var username = prompt("Please enter your Company Name.")
     var pettyCash = prompt("What's your monthly expense?");
-    console.log(annualBudget);
+    
+    console.log(pettyCash * month + " Dollars is your Annual Budget.");
+    //console.log(annualBudget);
     
 }
 budget();
+
+alert("Friendly reminder from Corporate: Please do not exceed your Annual Budget!")
 
 
 
