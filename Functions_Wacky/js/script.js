bf122e8b1f95ff94e0aff395b71d05b7c5a33652
raarjune <steffyarjune@gmail.com>
2013-12-18 23:32:42 -0500

    wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,5 +1,29 @@
 //Rajmatie Arjune, December 15, 2013 - Functions_Wacky
 
+function coldRemedy() {
+    var tea = 60;
+    var coffee = 70;
+
+//var cough = "tea";
+var userInput = prompt("What is the temperature?");
+
+if (tea > 65) {
+    console.log("Make yourself a cup of tea!...");
+    //code
+}else if (tea > 65) {
+    console.log("Make yourself a cup of coffee!");
+    //code
+}else{
+    console.log("Have orange juice instead!")
+        return("coldRemedy");
+}
+
+
+}
+
+//confirm("Hope you feel better soon!")
+
+coldRemedy();
 
 
 
