40147ff84cf817ded0fcd6f99c5bd4357d997f45
raarjune <steffyarjune@gmail.com>
2013-12-19 19:07:05 -0500

    Functions_Wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,30 +1,42 @@
 //Rajmatie Arjune, December 15, 2013 - Functions_Wacky
 
-function coldRemedy() {
-    var tea = 60;
-    var coffee = 70;
+//function coldRemedy() {
+//    var tea = 60;
+//    var coffee = 70;
+//
+////var cough = "tea";
+//var userInput = prompt("What is the temperature?");
+//
+//if (tea > 65) {
+//    console.log("Make yourself a cup of tea!...");
+//    //code
+//}else if (tea > 65) {
+//    console.log("Make yourself a cup of coffee!");
+//    //code
+//}else{
+//    console.log("Have orange juice instead!")
+//        return("coldRemedy");
+//}
+//
+//}
+//
+////confirm("Hope you feel better soon!")
+//
+//coldRemedy();
 
-//var cough = "tea";
-var userInput = prompt("What is the temperature?");
+//Calculating price tag from December 1st to December 31st Christmas Gifts if purchase everyday, or some days.
 
-if (tea > 65) {
-    console.log("Make yourself a cup of tea!...");
-    //code
-}else if (tea > 65) {
-    console.log("Make yourself a cup of coffee!");
-    //code
-}else{
-    console.log("Have orange juice instead!")
-        return("coldRemedy");
+function christmasGift(){
+    var gift = 0;
+    var day = 0;
+    var userPrice = prompt("How much are you planning to spend per gift?");
+    var userInfo = prompt("How many days will you buy presents?")
+
+    console.log(userPrice * userInfo + " Dollars is your price tag for the month.");
+    return"christmasGift"
+
 }
 
-
-}
-
-//confirm("Hope you feel better soon!")
-
-coldRemedy();
-
-
+christmasGift();
 
 
