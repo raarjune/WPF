86728002f5c06a6c5b4781dca10e2e4cef10f567
raarjune <steffyarjune@gmail.com>
2013-12-19 20:37:06 -0500

    Adding if statement

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,14 +1,17 @@
-//Rajmatie Arjune, December 15, 2013 - Functions_Personal
+//Rajmatie Arjune, December 19, 2013 - Functions_Personal
 //Mr Smith wants you to Calculate what he gives his kids in allowance for the year.
+//He has five kids and he gave each $15 allowance each week. 
 
-alert("This is a pop quiz on Mr.Smith!");
+alert("This is a pop quiz from Mr.Smith your good friend!");
 
 function kidsAllowance(allowance,children){
     //answer = allowance*children;
     //return answer;
 
-    var allowance = 5;
+    var allowance = 15;
+    
     var children = 5;
+    
     var weekly = 4;
     var month = 12;
     //var allowance = 0;
@@ -19,11 +22,20 @@ function kidsAllowance(allowance,children){
 
 var userInput = prompt("How many children does Mr.Smith have?");
 var userResult = prompt("How much does he give each child in allowance?");
-confirm("Mr. smith gives his kids a total of $1200 per year in allowance!");
+confirm("Mr. smith gives his kids a total of $3600. per year in allowance!");
 
 console.log(funds * year);
     return("kidsAllowance");
 }
 
 kidsAllowance();
+
+var allowance = 15;
+var children = 5;
+
+if (allowance > 10) {
+    
+console.log(children);
+    
+}
 
